8accbefd921f86794e3b0a90703890a5984f96fc
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-03-25 14:54:19 +0600

    Get Post Api

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:api_tester/models/posts_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,6 +10,18 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  List<PostsModel> postList = [];
+
+  Future<List<PostsModel>> getPostApi ()async{
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
+    if(response.statusCode == 200){
+
+    }else {
+
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
