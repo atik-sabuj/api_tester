a7c5225b7d341a60d9804ec7ae4d5dd9707bfbac
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-03-26 15:44:56 +0600

    Get User Api #2

--- lib/models/user_screen.dart
+++ lib/models/user_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:api_tester/models/user_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -10,6 +11,14 @@ class UserScreen extends StatefulWidget {
 }
 
 class _UserScreenState extends State<UserScreen> {
+
+  List<UserModel> userList = [];
+
+  Future<List<UserModel>> getUserApi ()async{
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
