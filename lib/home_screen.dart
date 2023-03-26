f1c969fde9408eaedf48cdfd3fc544baa4ecbf98
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-03-26 14:42:22 +0600

    Get Photo Api

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -32,7 +32,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Api Tester'),
+        title: Text('Api Posts'),
       ),
       body: Column(
         children: [
