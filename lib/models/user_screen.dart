307915a6f573addb90f0caaba843eb598cceadc1
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-03-26 15:49:41 +0600

    Get User Api #3

--- lib/models/user_screen.dart
+++ lib/models/user_screen.dart
@@ -17,7 +17,17 @@ class _UserScreenState extends State<UserScreen> {
   Future<List<UserModel>> getUserApi ()async{
     final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
 
+    var data = jsonDecode(response.body.toString());
 
+    if(response.statusCode == 200){
+      for(Map i in data){
+        print(i['name']);
+        userList.add(UserModel.fromJson(i));
+      }
+      return userList;
+    }else {
+      return userList;
+    }
   }
   @override
   Widget build(BuildContext context) {
