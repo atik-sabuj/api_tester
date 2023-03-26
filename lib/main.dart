4e7a5024a2cedd965cae22bc22602fc7d9e5fdf7
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-03-26 15:38:29 +0600

    Created User Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:api_tester/home_screen.dart';
+import 'package:api_tester/models/user_screen.dart';
 import 'package:api_tester/photo_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -19,7 +20,8 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       //home: const HomeScreen(),
-      home: const PhotoScreen(),
+      //home: const PhotoScreen(),
+      home: const UserScreen(),
     );
   }
 }
---
+++ lib/models/user_screen.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class UserScreen extends StatefulWidget {
+  const UserScreen({Key? key}) : super(key: key);
+
+  @override
+  State<UserScreen> createState() => _UserScreenState();
+}
+
+class _UserScreenState extends State<UserScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
